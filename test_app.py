fd05ce1f2976bdb036cd81355d644ed0f4cbfee4
Pratik Jade <76656961+pratikjade@users.noreply.github.com>
2024-09-03 16:46:02 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,6 +2,6 @@ def test_basic():
     assert 1 + 1 == 2
 
 def test_prediction():
-    from app import predict
+    from main import predict
     result = predict("This is a test")
     assert len(result) > 0
